a61149829951788aa9e19ec6a0a5d6cef3b24dc5
jeanetteong <jeanetteongjx@hotmail.com>
2026-08-16 22:29:08 +0800

    16/08 level-0 rename greet exit

---
+++ src/main/java/Jude.java
@@ -0,0 +1,12 @@
+public class Jude {
+    public static void main(String[] args) {
+        System.out.print("""
+            ____________________________________________________________
+            Hello! I'm Jude
+            What can I do for you?
+            ____________________________________________________________
+            Bye. Hope to see you again soon!
+            ____________________________________________________________    
+                """);
+    }
+}
