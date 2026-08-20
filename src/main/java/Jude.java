7a44e7698b5de3a7263260261d24f7079079613f
jeanetteong <jeanetteongjx@hotmail.com>
2026-08-21 01:33:41 +0800

    21/08 echo

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
 public class Jude {
     public static void main(String[] args) {
+        String line;
         System.out.print("""
-            ____________________________________________________________
-            Hello! I'm Jude
-            What can I do for you?
-            ____________________________________________________________
-            Bye. Hope to see you again soon!
-            ____________________________________________________________    
+                ____________________________________________________________
+                Hello! I'm Jude
+                What can I do for you?
+                ____________________________________________________________\n
+            """);
+        
+        while (true){
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        if (line.equals("bye")){
+            break;
+        }
+        System.out.printf("""
+                    ____________________________________________________________
+                    %s
+                    ____________________________________________________________\n
+                """,line);
+        }
+        
+        System.out.print("""
+                    ____________________________________________________________
+                    Bye. Hope to see you again soon!
+                    ____________________________________________________________    
+            
                 """);
     }
 }
